20695de1073c2544342d9e0ca06309461c54b928
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-11-01 01:14:15 -0600

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m3/3.1 Directed Cycle/3.1 Directed Cycle/Stack.java
+++ ADS-2-assignments/m3/3.1 Directed Cycle/3.1 Directed Cycle/Stack.java
@@ -1,38 +1,15 @@
-/*************************************************************************
- *  Compilation:  javac Stack.java
- *  Execution:    java Stack < input.txt
- *
- *  A generic stack, implemented using a linked list. Each stack
- *  element is of type Item.
- *  
- *  % more tobe.txt 
- *  to be or not to - be - - that - - - is
- *
- *  % java Stack < tobe.txt
- *  to be not that or be (2 left on stack)
- *
- *************************************************************************/
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
-
 /**
- *  The <tt>Stack</tt> class represents a last-in-first-out (LIFO) stack of generic items.
- *  It supports the usual <em>push</em> and <em>pop</em> operations, along with methods
- *  for peeking at the top item, testing if the stack is empty, and iterating through
- *  the items in LIFO order.
- *  <p>
- *  All stack operations except iteration are constant time.
- *  <p>
- *  For additional documentation, see <a href="/algs4/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * List of .
+ *
+ * @param      <Item>  The item
  */
 public class Stack<Item> implements Iterable<Item> {
     /**
-     * N.
+     * n.
      */
-    private int N;          // size of the stack
+    private int n;          // size of the stack
     /**
      *first.
      */
@@ -61,7 +38,7 @@ public class Stack<Item> implements Iterable<Item> {
      */
     public Stack() {
         first = null;
-        N = 0;
+        n = 0;
     }
 
    /**
@@ -85,7 +62,7 @@ public class Stack<Item> implements Iterable<Item> {
      * @return     { description_of_the_return_value }
      */
     public int size() {
-        return N;
+        return n;
     }
 
    /**
@@ -101,7 +78,7 @@ public class Stack<Item> implements Iterable<Item> {
         first = new Node();
         first.item = item;
         first.next = oldfirst;
-        N++;
+        n++;
     }
 
    /**
@@ -114,12 +91,12 @@ public class Stack<Item> implements Iterable<Item> {
      * @return     { description_of_the_return_value }
      */
     public Item pop() {
-        if (isEmpty()) { 
+        if (isEmpty()) {
         	throw new RuntimeException("Stack underflow");
         }
         Item item = first.item;        // save item to return
         first = first.next;            // delete first node
-        N--;
+        n--;
         return item;                   // return the saved item
     }
 
@@ -217,4 +194,9 @@ public class Stack<Item> implements Iterable<Item> {
         StdOut.println("(" + s.size() + " left on stack)");
     }*/
 }
+
+
+
+
+
 
