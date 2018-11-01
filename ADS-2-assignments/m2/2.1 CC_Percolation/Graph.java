6c38340caa7f6232b0fcdca47d11ae9db9ae6045
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-11-01 00:08:06 -0600

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m2/2.1 CC_Percolation/Graph.java
+++ ADS-2-assignments/m2/2.1 CC_Percolation/Graph.java
@@ -7,7 +7,7 @@ interface Graph {
     public int V();
     public int E();
     public void addEdge(int v, int w);
-    public Iterable<Integer> arr(int v);
+    //public Iterable<Integer> arr(int v);
     public boolean hasEdge(int v, int w);
     public Iterable<Integer> adj(int v);
 }
