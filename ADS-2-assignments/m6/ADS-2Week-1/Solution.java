41f80dd7dcbf6ff57da1896540ae94abf1bd8c26
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-11-03 13:00:48 -0600

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m6/ADS-2Week-1/Solution.java
+++ ADS-2-assignments/m6/ADS-2Week-1/Solution.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.io.*;
+import java.util.*;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Arrays;
 class PageRank {
@@ -5,13 +10,19 @@ class PageRank {
 	double pr;
 	PageRank(Digraph g) {
 		dg = g;
-		System.out.println("im");
-		pr = 1/dg.V();
-		System.out.println("hel");
+		System.out.println(dg.V() + "im cou");
+		pr = (1/dg.V());
+		System.out.println(pr);
+		//dg.matRep();
+		//System.out.println("hel");
+		//return false;
 		for(int i = 0; i < dg.V()-1; i++) {
+			// for(int k: adj(i)) {
+			// 	getPR(k);
+			// }
 			getPR(dg.V());
 		}
-		
+		//for(int k:adj[])
 		//System.out.println(pr);
 	}
 	public double getPR(int v) {
@@ -20,11 +31,12 @@ class PageRank {
 		for(int i = 0; i < 1000; i++) {
 			pr = pr/dg.outdegree(v);
 		}
+		//System.out.println(pr);
 		return pr;
 	}
 	public String toString() {
 		String str = "";
-		System.out.println("hi");
+		//System.out.println("hi");
 		for(int i = 0; i < dg.V()-1; i++) {
 			str = str + dg.V() + "-" + getPR(dg.V()) + "\n";
 		}
@@ -33,24 +45,64 @@ class PageRank {
 }
 
 class WebSearch {
+	HashMap<String, ArrayList<Integer>> h = new HashMap<String, ArrayList<Integer>>();
+	WebSearch(PageRank rank, String filename) throws Exception {
+		try {
+		File obj = new File("WebContent/" + filename);
+        Scanner fOne = new Scanner(obj);
+        while(fOne.hasNextLine()) {
+        	String[] tokens = fOne.nextLine().split(":");
+        	String[] words = tokens[1].split(" ");
+            for (int i = 0; i < words.length; i++) {
+                if (h.containsKey(words[i])) {
+                        ArrayList<Integer> arraylist = h.get(words[i]);
+                        arraylist.add(Integer.parseInt(tokens[0]));
+                } else {
+                        ArrayList<Integer> arraylist = new ArrayList<Integer>();
+                        arraylist.add(Integer.parseInt(tokens[0]));
+                        h.put(words[i], arraylist);
+                 }
+            }
 
+        }
+    }catch(Exception e) {
+    	System.out.println("Exception");
+    }
+	}
+	// public int iAmFeelingLucky(String query) {
+	// 	//String str1 = "";
+	// 	for(int i = 0; i < h.size(); i++) {
+	// 		if(h.containsKey(query[i])) {
+	// 			return h.get(query[i]);
+	// 		}
+	// 		else {
+	// 			return -1;
+	// 		}
+	// 	}
+	// 	return 0;
+	// }
 }
 
 
 public class Solution {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// read the first line of the input to get the number of vertices
 		Scanner s = new Scanner(System.in);
 		int vertices = Integer.parseInt(s.nextLine());
 		Digraph graph = new Digraph(vertices);
+		try {
+
+
 		while (s.hasNext()) {
             String[] n2 = s.nextLine().split(" ");//"0" "5"
             //System.out.println(Arrays.toString(n2));
-
+            for(int i = 0; i < n2.length; i++) {
             graph.addEdge(Integer.parseInt(n2[0]),//0,5
-                Integer.parseInt(n2[1]));
+                Integer.parseInt(n2[i]));
+        	}
         }
         System.out.println(graph.toString());
+    	//graph.matRep(vertices);
     	//System.out.println("hi");
 		// iterate count of vertices times 
 		// to read the adjacency list from std input
@@ -66,11 +118,15 @@ public class Solution {
 		// This part is only for the final test case
 		
 		// File path to the web content
-		String file = "WebContent.txt";
-		
+		String filetemp = "WebContent.txt";
+		//String file = "E:\\6053_ADS2\\6053_ADS-2\\ADS-2-assignments\\m6\\ADS-2Week-1\\" + filetemp;
+
+		//WebSearch search = new WebSearch(page,file);
 		// instantiate web search object
 		// and pass the page rank object and the file path to the constructor
-		
+		} catch(Exception e) {
+			System.out.println("Exception raised");
+		}
 		// read the search queries from std in
 		// remove the q= prefix and extract the search word
 		// pass the word to iAmFeelingLucky method of web search
