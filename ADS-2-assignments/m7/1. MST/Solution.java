e907052b8de0f27c1ad0f99531fb0c3ab8f4afc0
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-11-05 16:29:33 -0700

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m7/1. MST/Solution.java
+++ ADS-2-assignments/m7/1. MST/Solution.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//comstructor.
+	}
+	/**
+	 * main.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		int vertices = Integer.parseInt(s.nextLine());
 		int edges = Integer.parseInt(s.nextLine());
