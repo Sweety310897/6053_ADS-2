49e010cf79d7e156aff7ce01cf392d02e32d64b1
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-11-08 11:13:33 -0700

    "ads2_m10_a1_1: 2/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m10/Seam Carving/SeamCarver.java
+++ ADS-2-assignments/m10/Seam Carving/SeamCarver.java
@@ -1,26 +1,36 @@
 
 public class SeamCarver {
 	// create a seam carver object based on the given picture
+	Picture p;
+	private int width;
+	private int height;
+	int[][] energyvar;
 	public SeamCarver(Picture picture) {
-
+		this.p = picture;
+		this.width = p.width();
+		this.height = p.height();
 	}
 	// current picture
 	public Picture picture() {
-		return null;
+		return p;
 	}
 	// width of current picture
 	public int width() {
-		return 0;
+		return width;
 	}
 
 	// height of current picture
 	public int height() {
-		return 0;
+		return height;
 	}
 
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
-		return 0;
+		//return 0;
+		//int[][] matrix = new int[x][y];
+		//if()
+		return energyvar[y][x];
+		
 	}
 
 	// sequence of indices for horizontal seam
