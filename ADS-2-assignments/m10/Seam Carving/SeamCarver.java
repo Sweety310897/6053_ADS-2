3e8cb2a196d39658f2b328f6d057ede55193fe50
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-11-08 14:52:31 -0700

    "ads2_m10_a1_1: 2/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m10/Seam Carving/SeamCarver.java
+++ ADS-2-assignments/m10/Seam Carving/SeamCarver.java
@@ -27,14 +27,28 @@ public class SeamCarver {
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
 		// System.out.println("xval" + x + " " + "yval" + y);
-		// if(x == 0 || y == 0 || x == height()-1 || y == width()-1) {
-		// 	energymat[x][y] = 1000;
-		// }
-		// else {
-
-		// }
-		// return energymat[x][y];
-		return 0;
+		if(x == 0 || y == 0 || x == height()-1 || y == width()-1) {
+			return 1000;
+		}
+		double tempx;
+		double tempy;
+		double xtemp1red = p.get(x - 1,y).getRed();
+		double xtemp1blue = p.get(x - 1,y).getBlue();
+		double xtemp1green = p.get(x - 1,y).getGreen();
+		double xtemp2red = p.get(x + 1,y).getRed();
+		double xtemp2blue = p.get(x + 1,y).getBlue();
+		double xtemp2green = p.get(x + 1,y).getGreen();
+		double ytemp1red = p.get(x ,y-1).getRed();
+		double ytemp1blue = p.get(x ,y-1).getBlue();
+		double ytemp1green = p.get(x ,y-1).getGreen();
+		double ytemp2red = p.get(x , y+1).getRed();
+		double ytemp2blue = p.get(x ,y+1).getBlue();
+		double ytemp2green = p.get(x ,y+1).getGreen();
+		tempx = Math.pow(xtemp1red - xtemp2red, 2) + Math.pow(xtemp1blue - xtemp2blue, 2) +Math.pow(xtemp1green - xtemp2green , 2);
+		tempy = Math.pow(ytemp1red - ytemp2red, 2) + Math.pow(ytemp1blue - ytemp2blue, 2) +Math.pow(ytemp1green - ytemp2green , 2);
+		double energysum = Math.sqrt(tempx + tempy);
+		return energysum;
+		//return 0;
 	}
 
 	// sequence of indices for horizontal seam
