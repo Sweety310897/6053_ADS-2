c7aacc683917c3b360c0357ccf6bc27adf2ca421
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-11-11 21:36:07 -0700

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m10/Seam Carving/ShowSeams.java
+++ ADS-2-assignments/m10/Seam Carving/ShowSeams.java
@@ -1,35 +1,37 @@
-/******************************************************************************
- *  Compilation:  javac ShowSeams.java
- *  Execution:    java ShowSeams input.png
- *  Dependencies: SeamCarver.java SCUtility.java
- *
- *  Read image from file specified as command line argument. Show 3 images 
- *  original image as well as horizontal and vertical seams of that image.
- *  Each image hides the previous one - drag them to see all three.
- *
- ******************************************************************************/
-
 import edu.princeton.cs.algs4.Picture;
 import edu.princeton.cs.algs4.StdOut;
-
+/**
+ * Class for show seams.
+ */
 public class ShowSeams {
-
-    private static void showHorizontalSeam(SeamCarver sc) {
+    /**
+     * Shows the horizontal seam.
+     *
+     * @param      sc    The screen
+     */
+    private static void showHorizontalSeam(final SeamCarver sc) {
         Picture picture = SCUtility.toEnergyPicture(sc);
         int[] horizontalSeam = sc.findHorizontalSeam();
         Picture overlay = SCUtility.seamOverlay(picture, true, horizontalSeam);
         overlay.show();
     }
-
-
-    private static void showVerticalSeam(SeamCarver sc) {
+    /**
+     * Shows the vertical seam.
+     *
+     * @param      sc    The screen
+     */
+    private static void showVerticalSeam(final SeamCarver sc) {
         Picture picture = SCUtility.toEnergyPicture(sc);
         int[] verticalSeam = sc.findVerticalSeam();
         Picture overlay = SCUtility.seamOverlay(picture, false, verticalSeam);
         overlay.show();
     }
-
-    public static void main(String[] args) {
+    /**
+     * main.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Picture picture = new Picture(args[0]);
         StdOut.printf("image is %d columns by %d rows\n", picture.width(), picture.height());
         picture.show();        
@@ -40,7 +42,5 @@ public class ShowSeams {
 
         StdOut.printf("Displaying vertical seam calculated.\n");
         showVerticalSeam(sc);
-
     }
-
 }
--- ADS-2-assignments/m10/Seam Carving/Solution.java
+++ ADS-2-assignments/m10/Seam Carving/Solution.java
@@ -161,3 +161,7 @@ public final class Solution {
 
     }
 }
+
+
+
+
