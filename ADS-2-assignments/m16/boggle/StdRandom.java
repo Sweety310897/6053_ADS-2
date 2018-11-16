0381d2965c36aed4a5145877291defee65661440
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-11-16 16:35:37 -0700

    "ads2_m16_a1_1: 30/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m16/boggle/StdRandom.java
+++ ADS-2-assignments/m16/boggle/StdRandom.java
@@ -516,18 +516,19 @@ public final class StdRandom {
         double[] probabilities = { 0.5, 0.3, 0.1, 0.1 };
         int[] frequencies = { 5, 3, 1, 1 };
         String[] a = "A B C D E F G".split(" ");
-
+        final float two = 0.2;
         System.out.println("seed = " + StdRandom.getSeed());
         for (int i = 0; i < n; i++) {
             StdOut.printf("%2d ",   uniform(100));
             StdOut.printf("%8.5f ", uniform(10.0, 99.0));
             StdOut.printf("%5b ",   bernoulli(0.5));
-            StdOut.printf("%7.5f ", gaussian(9.0, 0.2));
+            StdOut.printf("%7.5f ", gaussian(9.0, two));
             StdOut.printf("%1d ",   discrete(probabilities));
             StdOut.printf("%1d ",   discrete(frequencies));
             StdRandom.shuffle(a);
-            for (String s : a)
+            for (String s : a) {
                 StdOut.print(s);
+            }
             System.out.println();
         }
     }
